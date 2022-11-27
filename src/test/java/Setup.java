546e46639a6f96ecf4e7d11cd2b86799b95ccf41
Justyna <justyna.m.ramotowska@gmail.com>
2022-11-27 17:45:31 +0100

    Added files

---
+++ src/test/java/Setup.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Setup {
+    String projectLocation = System.getProperty("user.dir");
+    WebDriver driver;
+
+    @BeforeEach
+    public void beforeTest(){
+        System.out.println(projectLocation);
+        System.setProperty("webdriver.chrome.driver", projectLocation+"/resource/chromedriver.exe");
+
+        //otwórz przeglądarkę
+        driver = new ChromeDriver();
+
+        //przejdź do strony QA
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+    }
+
+    @AfterEach
+    public void afterTest(){
+        //zamknij przeglądarkę
+        driver.close();
+
+    }
+}
